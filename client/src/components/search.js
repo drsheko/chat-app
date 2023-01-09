4e960f9e2d87a10b1e50d986a946cf35abbf3674
drsheko <drsheko@gmail.com>
2023-01-08 21:09:57 -0600

    modify search style

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,98 +1,162 @@
-import React from 'react';
-import {useState, useEffect, useContext} from 'react'
-import axios from 'axios'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container'
-import CloseButton from 'react-bootstrap/CloseButton'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row';
-import { UserContext } from '../App';
-import ContactCard from './contactCard';
-
+import React from "react";
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import CloseButton from "react-bootstrap/CloseButton";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { UserContext } from "../App";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import {
+  BootstrapDialog,
+  BootstrapDialogTitle,
+} from "../styled_components/bootstrapDialog";
+import {
+  DialogContent,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 
 function Search(props) {
-    var user = useContext(UserContext)
-    const [form, setForm] = useState('')
-    const [resultOpen, setResultOpen] = useState(false);
-    const [ searchResult, setSearchResult] = useState([]);
+  var { user } = useContext(UserContext);
+  const [form, setForm] = useState("");
+  const [resultOpen, setResultOpen] = useState(false);
+  const [searchResult, setSearchResult] = useState([]);
 
-    const handleChange = (e) => {
-        setForm(e.target.value)
+  const handleChange = (e) => {
+    setForm(e.target.value);
+  };
+  const closeResult = () => {
+    setResultOpen(false);
+  };
+  const handleClose = () => {
+    setResultOpen(false);
+  };
+  const addFriend = async (e) => {
+    e.preventDefault();
+    try {
+      var contactId = searchResult[0]._id;
+      var url = `http://localhost:3001/api/${user._id}/${contactId}`;
+      var res = await axios.post(url);
+
+      var data = await res.data;
+      props.setAddedFriend(data.addedFriend);
+      closeResult();
+    } catch {}
+  };
+  const searchContacts = async (e) => {
+    e.preventDefault();
+    try {
+      var url = "http://localhost:3001/api/contacts-search";
+
+      var res = await fetch(url, {
+        method: "Post",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          search: form,
+        }),
+      });
+      var data = await res.json();
+      setResultOpen(true);
+      setSearchResult(data.result);
+      setForm("");
+    } catch (err) {
+      console.log(err);
     }
-    const closeResult = () => {
-        setResultOpen(false)
-    }
-    const addFriend = async (e) => {
-        e.preventDefault();
-        try {
-            var contactId = searchResult[0]._id
-            var url = `http://localhost:3001/api/${user._id}/${contactId}`;
-            var res = await axios.post(url)
-        
-            var data = await res.data
-            props.setAddedFriend(data.addedFriend);
-            closeResult()
-          
-        } catch {}
-      };
-    const searchContacts = async(e) => {
-        e.preventDefault(); 
-        try{ 
-            var url = "http://localhost:3001/api/contacts-search";
-           
-            var res = await fetch(url, {
-                method:'Post',
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                  },
-                body: new URLSearchParams({
-                    search: form
-                })
-            })
-            var data =await res.json()
-            setResultOpen(true)
-            setSearchResult(data.result)
-            setForm('')
-        }
-        catch(err){
-            console.log(err)
-        }
-        
-    } 
-    return (
-        <div>
-        <Form onSubmit={searchContacts}> 
-           <Container>
-                <Row>
-                    <Col>
-                        <Form.Control type='search' value={form} onChange={handleChange} placeholder='search ....'  required/> 
-                    </Col>
-                    <Col>
-                        <Button type="submit" variant='secondary'><i className="bi bi-search"></i></Button>
-                    </Col>                
-                </Row>
-           </Container>
-        </Form>
-        {
-            resultOpen?
-            <Container >
-                <CloseButton  onClick={closeResult}/>
-                {searchResult.length>0?
-                    searchResult.map(e=>
-                        <>
-                            <div>{e.username}</div>
-                            {user.friends.includes(e._id)?''
-                            :<button onClick={addFriend} >Add</button>}
-                        </>
-                    )
-                    :'No item found'
-                }
-            </Container>
-            :''
-        }      
-        </div>
-    );
+  };
+  return (
+    <div>
+      <Form onSubmit={searchContacts} className="mb-3">
+        <OutlinedInput
+          value={form}
+          onChange={handleChange}
+          placeholder="Search...."
+          required
+          fullWidth
+          color="info"
+          size="small"
+          style={{ backgroundColor: "#f5f5f5" }}
+          id="outlined-adornment-weight"
+          startAdornment={<i className="bi bi-search me-3"></i>}
+          endAdornment={
+            <Button
+              type="submit"
+              className="text-capitalize"
+              variant="contained"
+              color="info"
+              style={{
+                position: "absolute",
+                right: "0",
+                height: "100%",
+                borderTopLeftRadius: "0",
+                borderBottomLeftRadius: "0",
+              }}
+            >
+              Search
+            </Button>
+          }
+          aria-describedby="outlined-weight-helper-text"
+          inputProps={{
+            "aria-label": "weight",
+          }}
+        />
+      </Form>
+
+      {resultOpen ? (
+        <BootstrapDialog
+          onClose={handleClose}
+          aria-labelledby="customized-dialog-title"
+          open={resultOpen}
+          fullWidth={true}
+          maxWidth="sm"
+        >
+          <BootstrapDialogTitle
+            id="customized-dialog-title"
+            onClose={handleClose}
+            className="text-center"
+          >
+            Search
+          </BootstrapDialogTitle>
+          <DialogContent dividers>
+            <Typography>We found {searchResult.length} result(s).</Typography>
+
+            {searchResult.length > 0
+              ? searchResult.map((e) => (
+                  <div className="d-flex flex-justify-between">
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar alt={e.username} src={e.avatarURL} />
+                      </ListItemAvatar>
+                      <ListItemText primary={e.username} />
+                    </ListItem>
+                    {user.friends.includes(e._id) ? (
+                      ""
+                    ) : (
+                      <Button
+                        size="small"
+                        onClick={addFriend}
+                        className="text-capitalize"
+                      >
+                        Follow
+                      </Button>
+                    )}
+                  </div>
+                ))
+              : "No item found"}
+          </DialogContent>
+        </BootstrapDialog>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 export default Search;
