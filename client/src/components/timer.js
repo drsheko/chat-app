5ba8dc2f5d5e6f8ee740d6e1d2a23ac518eed175
drsheko <drsheko91@gmail.com>
2023-02-11 23:27:51 -0600

    fix call timer

--- client/src/components/timer.js
+++ client/src/components/timer.js
@@ -1,37 +1,37 @@
-import React, { useEffect } from 'react';
-import { useStopwatch } from 'react-timer-hook'
+import React, { useEffect } from "react";
+import { useStopwatch } from "react-timer-hook";
 function Timer(props) {
-    const {
-        seconds,
-        minutes,
-        hours,
-        days,
-        isRunning,
-        
-      } = useStopwatch({ autoStart: true });
-    
-    useEffect(()=>{
-     // props.setDuration(`${hours} : ${minutes} : ${seconds}`);
-      var callSummary 
-      if(hours ==0 &&minutes==0){
-        callSummary = `${seconds} secs`
-      }
-      else if(hours==0){
-         callSummary =`${minutes} mins ${seconds} secs`
-      }else{
-         callSummary =`${hours} hr ${minutes} mins ${seconds} secs`
-      }
-      props.setDuration(callSummary)
-    },[seconds,minutes,hours])
-      return (
-        <div style={{textAlign: 'center' , position:'absolute', top:'0', left:'0' ,backgroundColor:'white'}}>
-          <div style={{fontSize: '20px'}}>
-           <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
-          </div>
-          
-          
-        </div>
-      );
+  const { seconds, minutes, hours, days, isRunning } = useStopwatch({
+    autoStart: true,
+  });
+
+  useEffect(() => {
+    var callSummary;
+    if (hours == 0 && minutes == 0) {
+      callSummary = `${seconds} secs`;
+    } else if (hours == 0) {
+      callSummary = `${minutes} mins ${seconds} secs`;
+    } else {
+      callSummary = `${hours} hr ${minutes} mins ${seconds} secs`;
+    }
+    props.setDuration(callSummary);
+  }, [seconds, minutes, hours]);
+  return (
+    <div
+      style={{
+        padding: "3px",
+        margin: "5px",
+        borderRadius: "5px",
+        fontSize: "16px",
+        backgroundColor: "gray",
+        color: "white",
+      }}
+    >
+      <span>{hours === 0 ? "" : `${hours}:`}</span>
+      <span>{minutes === 0 ? "0:" : `${minutes}:`}</span>
+      <span>{seconds}</span>
+    </div>
+  );
 }
 
 export default Timer;
