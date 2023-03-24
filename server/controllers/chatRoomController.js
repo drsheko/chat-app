c0b9f391b09baad89a0afa14f91f7e24ceee4415
drsheko <drsheko91@gmail.com>
2023-03-23 21:12:51 -0500

    clean up code

--- server/controllers/chatRoomController.js
+++ server/controllers/chatRoomController.js
@@ -1,50 +1,45 @@
-var mongoose = require('mongoose');
-var User = require('../models/userModel')
-var ChatRoom =require('../models/chatRoomModel');
-var Message = require('../models/messageModel');
+var mongoose = require("mongoose");
+var User = require("../models/userModel");
+var ChatRoom = require("../models/chatRoomModel");
+var Message = require("../models/messageModel");
 
-exports.initiateChat = async(req,res) => {
-   var userIds = req.body.userIds
-    try{
-        var chatRoom =await ChatRoom.initiateChat(userIds)
-        return res.status(200).json({ success: true, chatRoom });}
-    catch(error){
-        return res.status(500).json({success:false,error})
-    }
-}
+exports.initiateChat = async (req, res) => {
+  var userIds = req.body.userIds;
+  try {
+    var chatRoom = await ChatRoom.initiateChat(userIds);
+    return res.status(200).json({ success: true, chatRoom });
+  } catch (error) {
+    return res.status(401).json({ success: false, error });
+  }
+};
 
-exports.get_chatRoomByRoomId = async(req,res) => {
-    var chatId =req.body.roomId;
-    try{    
-        var chatRoom = ChatRoom.findById(chatId)
-            .populate({
-                path:'messages',
-                populate:{
-                    path:'postedBy'
-                }
-            })
-           .exec( (error,room)=>{
-                if (error){  return res.status(500).json({success:false, error})
-                   
-                } 
-                return res.status(200).json({success:true, room})
-            })
+exports.get_chatRoomByRoomId = async (req, res) => {
+  var chatId = req.body.roomId;
+  try {
+    var chatRoom = ChatRoom.findById(chatId)
+      .populate({
+        path: "messages",
+        populate: {
+          path: "postedBy",
+        },
+      })
+      .exec((error, room) => {
+        if (error) {
+          return res.status(500).json({ success: false, error });
+        }
+        return res.status(200).json({ success: true, room });
+      });
+  } catch (error) {
+    return res.status(500).json({ success: false, error });
+  }
+};
 
-       /* var chatRoom = await ChatRoom.getChatRoomByRoomId(chatId);
-        return res.status(200).json({success:true, chat})*/
-    }
-    catch(error){
-        return res.status(500).json({success:false, error})
-    }
-}
-
-exports.getAllChatRoomsByUserId = async(req,res) => {
-    var userId = req.params.userid;
-    try{
-        var rooms = await ChatRoom.getChatRoomsByUserId(userId);
-        res.status(200).json({success:true, rooms})
-    }
-    catch(error){
-        res.status(500).json({success:false, error})
-    }
-}
+exports.getAllChatRoomsByUserId = async (req, res) => {
+  var userId = req.params.userid;
+  try {
+    var rooms = await ChatRoom.getChatRoomsByUserId(userId);
+    res.status(200).json({ success: true, rooms });
+  } catch (error) {
+    res.status(401).json({ success: false, error });
+  }
+};
